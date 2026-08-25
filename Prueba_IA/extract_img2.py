d1225e4ce52b307aa0718a75c789dfa4f22a110e
isiayala <isi.ayala@duocuc.cl>
2026-08-24 22:29:47 -0400

    Remueve Prueba_IA definitivamente del repo de portafolio

--- Prueba_IA/extract_img2.py
+++
@@ -1,9 +0,0 @@
-import re
-with open('onedrive_page.html', 'r', encoding='utf-8', errors='ignore') as f:
-    content = f.read()
-img_tags = re.findall(r'<img[^>]*src="([^"]+)"[^>]*>', content)
-for src in img_tags[:20]:
-    print(src)
-data_src = re.findall(r'data-src="([^"]+)"', content)
-for s in data_src[:10]:
-    print(s)
